a1394c64be6ed189c27360c3920d27cb857894a5
hooman130 <ec2-user@ip-172-31-67-41.ec2.internal>
2020-09-26 01:50:20 +0000

    final

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -4,8 +4,21 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create!(movie)
   end
-  fail "Unimplemented"
+  #fail "Unimplemented"
+end
+
+When /^I follow"(.*)$"/ do |sort_choice|
+  case sort_choice
+  when "Movie Title"
+    click_on("title_header")
+  when "Release Date"
+    click_on("release_date_header")
+  end
+  # if sort_choice=="Movie Title"
+  #   click_on("title_header")
+  # end
 end
 
 Then /(.*) seed movies should exist/ do | n_seeds |
@@ -18,7 +31,14 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  fail "Unimplemented"
+  pb = page.body.to_s
+  if pb.index(e1)!=nil && pb.index(e2)!=nil
+    if pb.index(e1) >= pb.index(e2)
+      assert false, "jr_fail"
+    end
+  else
+    assert false, "jr_fail"
+  end
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -29,10 +49,18 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  rating_list.split(',').each do |rating|
+    rating.strip!
+    if uncheck
+      uncheck("ratings_#{rating}")
+    else
+      check("ratings_#{rating}")
+    end
+  end
 end
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
+  rows = page.all('#movies tr').size - 1
+  rows.should == Movie.count
 end
